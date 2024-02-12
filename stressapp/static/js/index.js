64eff6a3e9353599691a136b8c25cec5b6ff3d6b
Caden-Hornyak <108236501+Caden-Hornyak@users.noreply.github.com>
2024-02-11 21:51:21 -0800

    page styling and auth completion

--- stressapp/static/js/index.js
+++ stressapp/static/js/index.js
@@ -3,41 +3,55 @@ let right = document.querySelector('.right');
 let authDisp = false;
 let clone;
 
+
 left.onclick = function () {
+    let loadreg = document.querySelector(".loadreg");
     left.classList.toggle('active');
     right.classList.toggle('deactive');
     if (!authDisp) {
         clone = $("#loadreg").clone(true, true);
+        loadreg.classList.toggle('active');
         $.get("signup", function(data){
         $("#register").replaceWith(data);
         });
+
+        // wait for form load before applying tag select
+        setTimeout(function() {
+            new MultiSelectTag('countries');
+        }, 30);
+        
         authDisp = true;
     } else {
         $("#loadreg").replaceWith(clone);
+        loadreg.classList.toggle('active');
         authDisp = false;
     }
     
 };
 
 right.onclick = function () {
+    let loadlog = document.querySelector(".loadlog");
     left.classList.toggle('deactive');
     right.classList.toggle('active');
     
     if (!authDisp) {
         clone = $("#loadlog").clone(true, true);
         $.get("signin", function(data){
-            console.log("hello");
             $("#signin").replaceWith(data);
         });
+        loadlog.classList.toggle('active');
         authDisp = true;
     } else {
+        loadlog.classList.toggle('active');
         $("#loadlog").replaceWith(clone);
         authDisp = false;
     }
 };
 
-function clearcontent(elementID) { 
-    document.querySelector('.left').innerHTML = ""; 
-} 
+
+
+
+
+
 
 
--- stressapp/views.py
+++ stressapp/views.py
@@ -19,23 +19,23 @@ def signup(request):
 
         if User.objects.filter(username=username):
             messages.error(request, "Username already exist! Please try some other username.")
-            return redirect('home')
+            return redirect('index')
         
         if User.objects.filter(email=email).exists():
             messages.error(request, "Email Already Registered!")
-            return redirect('home')
+            return redirect('index')
         
         if len(username)>20:
             messages.error(request, "Username must be under 20 charcters!")
-            return redirect('home')
+            return redirect('index')
         
         if pass1 != pass2:
             messages.error(request, "Passwords didn't matched!")
-            return redirect('home')
+            return redirect('index')
         
         if not username.isalnum():
             messages.error(request, "Username must be Alpha-Numeric!")
-            return redirect('home')
+            return redirect('index')
         
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
@@ -60,14 +60,14 @@ def signin(request):
         if user is not None:
             login(request, user)
             fname = user.first_name
-            return render(request, "index.html", {"fname":fname})
+            return render(request, "index.html")
         else:
             messages.error(request, "Bad Credentials!")
-            return redirect('home')
+            return redirect('index')
         
     return render(request, "signin.html")
 
 def signout(request):
     logout(request)
     messages.success(request, "Logged Out Successfully!!")
-    return redirect('home')
+    return redirect('index')
